fe04077dcebd79417ea33eb41f806a909251eff6
Ranieri Althoff <ranisalt@gmail.com>
2015-03-24 03:51:42 -0300

    Copy files instead of linking, which does not work

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env fish
 set -l dest ~/.local/bin
-set -l pwd (pwd)
 if test -d $dest
 	mkdir -p $dest
 end
 
 for i in (find -maxdepth 1 -type f -executable ! -iname "install.sh" -printf "%f\n")
-	ln -sfv "$pwd/$i" "$dest/$i"
+	cp -fv "$i" "$dest/$i"
 end
