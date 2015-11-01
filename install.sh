8e5da9c25fd1f2b24e9f12d852b9f974ac5f8b6e
Ranieri Althoff <ranisalt@gmail.com>
2015-11-01 19:20:08 -0200

    Add i3blocks scripts

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ end
 for i in (find -maxdepth 1 -type f -executable ! -iname "install.sh" -printf "%f\n")
 	cp -fv "$i" "$dest/$i"
 end
+
+for i in (find -maxdepth 1 -type d -executable ! -name ".*" -printf "%f\n")
+    mkdir -pv "$dest/$i"
+	cp -frv "$i" "$dest"
+end
