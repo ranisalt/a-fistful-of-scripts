7d282bc822d7bcbbd9e7122d652422c0d3d885a9
Ranieri Althoff <ranisalt@gmail.com>
2015-11-20 22:09:50 -0200

    Use unaliased cp to prevent messed flags

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ if [ ! -d $dest ]
 end
 
 for i in (find -maxdepth 1 -type f -executable ! -iname "install.sh" -printf "%f\n")
-	cp -fv "$i" "$dest/$i"
+	command cp -fv "$i" "$dest/$i"
 end
 
 for i in (find -maxdepth 1 -type d -executable ! -name ".*" -printf "%f\n")
     mkdir -pv "$dest/$i"
-	cp -frv "$i" "$dest"
+	command cp -frv "$i" "$dest"
 end
