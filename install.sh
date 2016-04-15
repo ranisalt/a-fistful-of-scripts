f6e26a465b113f8c7c6879b7267164025412b782
Ranieri Althoff <ranisalt@gmail.com>
2016-04-15 19:23:41 -0300

    Use sh & rsync based install script

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
-#!/usr/bin/env fish
-set -l dest $HOME/.local/bin
-if [ ! -d $dest ]
-	mkdir -pv $dest
-end
+#!/usr/bin/env sh
+DESTDIR=$HOME/.local/bin
+if [ ! -d $DESTDIR ]; then
+    mkdir -pv $DESTDIR
+fi
 
-for i in (find -maxdepth 1 -type f -executable ! -iname "install.sh" -printf "%f\n")
-	command cp -fv "$i" "$dest/$i"
-end
-
-for i in (find -maxdepth 1 -type d -executable ! -name ".*" -printf "%f\n")
-    mkdir -pv "$dest/$i"
-	command cp -frv "$i" "$dest"
-end
+for i in *; do
+	rsync -a "$i" "$DESTDIR/$i"
+done
